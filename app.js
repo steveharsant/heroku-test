a103b431a925a658c430c3c44f5293b91817cd17
Steve Harsant <stevenharsant@gmail.com>
2020-08-19 12:47:38 +0100

    breaking change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
-
+THIS WILL BREAK THE APP!
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
