008f12db417eedfe8ec65af2625b6cbd1425b880
Steve Harsant <stevenharsant@gmail.com>
2020-08-19 13:45:36 +0100

    fail

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-co2nst express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+// co2nst express = require('express')
+// const app = express()
+// const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.se4nd('Hello, world!')
-})
+// app.get('/', (req, res) => {
+//   res.se4nd('Hello, world!')
+// })
 
-app.listen(port, () => {
-  con4sole.log(`Example app listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//   con4sole.log(`Example app listening at http://localhost:${port}`)
+// })
