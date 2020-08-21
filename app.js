f4aa9d45b38507daf2c83faba24e07a29f003221
Steve Harsant <stevenharsant@gmail.com>
2020-08-21 12:12:40 +0100

    test

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// co2nst express = require('express')
-// const app = express()
-// const port = process.env.PORT || 3000
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
-// app.get('/', (req, res) => {
-//   res.se4nd('Hello, world!')
-// })
+app.get('/', (req, res) => {
+  res.send('Hello, worlds?')
+})
 
-// app.listen(port, () => {
-//   con4sole.log(`Example app listening at http://localhost:${port}`)
-// })
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
